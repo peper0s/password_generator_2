724d5f7b1e24fb70f4a7bc5aa5c25f128f30b51c
peper0s <timgrinkevich@gmail.com>
2023-11-28 11:46:11 +0600

    create main file

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+password_keys = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+password_numbers = int(input("введите длину пароля"))
+
+password = ""
+
+for i in range(password_numbers):
+    letter = random.choice(password_keys)
+    password += letter
+print(password)
